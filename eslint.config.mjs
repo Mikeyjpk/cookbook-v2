d18b5d08372378f9e703ec44cc82a9bf9f6dc667
Mikeyjpk <mikey.kerr@truescope.com>
2025-01-30 19:51:14 +1100

    lint update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,21 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
+// Check if we are in development mode
+const isDev = process.env.NODE_ENV === "development";
+
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+	...compat.extends("next/core-web-vitals", "next/typescript"),
+	{
+		rules: {
+			"@typescript-eslint/no-explicit-any": "off",
+			"no-var": "off",
+			"@typescript-eslint/no-unused-vars": "off",
+		},
+	},
 ];
 
 export default eslintConfig;
